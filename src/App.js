d3f823f6493a6dc74424516075d4ddde5ab71d04
bad-news-everyone <miss.budnik@list.ru>
2020-03-22 12:49:58 +0300

    kk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ''
+    };
+  }
+   render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+    <div className='wrapper'>
+      <Input />
+      <div className='row'>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>*</button>
+      </div>
+      <div className='row'>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+      </div>
+      <div className='row'>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+      </div>
+      <div className='row'>
+        <button>C</button>
+        <button>0</button>
+        <button>/</button>
+        <button>=</button>
+      </div>
+    </div>
     </div>
   );
+   }
 }
+
 
 export default App;
