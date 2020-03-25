abe884c76b5818825599abea0e18aef33d60804a
bad-news-everyone <miss.budnik@list.ru>
2020-03-25 22:45:58 +0300

    kk

--- src/App.js
+++ src/App.js
@@ -11,39 +11,45 @@ class App extends React.Component {
       input: ''
     };
   }
-   render() {
-  return (
-    <div className='app'>
-    <div className='wrapper'>
-      <Input />
-      <div className='row'>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
+
+  addToInput = val => {
+    this.setState({ input: this.state.input + val })
+  };
+
+
+  render() {
+    return (
+      <div className='app'>
+        <div className='wrapper'>
+          <Input input={this.state.input} />
+          <div className='row'>
+            <button onClick={() => this.addToInput('7')}>7</button>
+            <button onClick={() => this.addToInput('8')}>8</button>
+            <button onClick={() => this.addToInput('9')}>9</button>
+            <button onClick={() => this.addToInput('*')}>*</button>
+          </div>
+          <div className='row'>
+            <button onClick={() => this.addToInput('4')}>4</button>
+            <button onClick={() => this.addToInput('5')}>5</button>
+            <button onClick={() => this.addToInput('6')}>6</button>
+            <button onClick={() => this.addToInput('-')}>-</button>
+          </div>
+          <div className='row'>
+            <button onClick={() => this.addToInput('1')}>1</button>
+            <button onClick={() => this.addToInput('2')}>2</button>
+            <button onClick={() => this.addToInput('3')}>3</button>
+            <button onClick={() => this.addToInput('+')}>+</button>
+          </div>
+          <div className='row'>
+            <button>C</button>
+            <button onClick={() => this.addToInput('0')}>0</button>
+            <button onClick={() => this.addToInput('/')}>/</button>
+            <button onClick={() => this.addToInput('=')}>=</button>
+          </div>
+        </div>
       </div>
-      <div className='row'>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-      </div>
-      <div className='row'>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-      </div>
-      <div className='row'>
-        <button>C</button>
-        <button>0</button>
-        <button>/</button>
-        <button>=</button>
-      </div>
-    </div>
-    </div>
-  );
-   }
+    );
+  }
 }
 
 
